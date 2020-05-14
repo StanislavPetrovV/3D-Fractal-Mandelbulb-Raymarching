5127dccc17a84d32b4b48428ad06859d3e37f8c1
StanislavPetrovV <34447380+StanislavPetrovV@users.noreply.github.com>
2020-05-14 12:53:55 +0800

    refactoring

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from settings import *
 import control
 from multiprocessing import Pool
 
-
 if __name__ == '__main__':
     os.environ['SDL_VIDEO_WINDOW_POS'] = f'{(tk.Tk().winfo_screenwidth() - WIDTH) // 2},' \
                                          f'{(tk.Tk().winfo_screenheight() - HEIGHT) // 4}'
@@ -19,30 +18,33 @@ if __name__ == '__main__':
     pygame.mouse.set_visible(False)
     control = control.Control()
 
-    # fps = []
-    start = time()
-    game = True
-    for i in range(10):
-    # while game:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                # print(sum(fps) / len(fps))
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    game = False
-        control.pressed_keys()
-        sc.fill(DARKGRAY)
+    with Pool(processes=4) as pool:
+        start = time()
+        game = True
+        while game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game = False
+            control.pressed_keys()
+            sc.fill(DARKGRAY)
 
-        # t = time() * 0.25
-        for color,x,y in ray_marching(control.key):
-            pygame.draw.rect(sc, color, (x, y, control.key[5], control.key[5]))
+            res_0 = pool.apply_async(ray_marching, (control.key, 0))
+            res_1 = pool.apply_async(ray_marching, (control.key, 1))
+            res_2 = pool.apply_async(ray_marching, (control.key, 2))
+            res_3 = pool.apply_async(ray_marching, (control.key, 3))
 
-        pygame.display.flip()
-        clock.tick()
-        # fps.append(int(clock.get_fps()))
-    # print(sum(fps) / len(fps))
+            result = res_0.get() + res_1.get() + res_2.get() + res_3.get()
+
+            side = control.key[5] * 2
+            for color, x, y in result:
+                pygame.draw.rect(sc, color, (x, y, control.key[5], side))
+
+            pygame.display.flip()
+            clock.tick()
     print(time() - start)
     pygame.quit()
     sys.exit()
